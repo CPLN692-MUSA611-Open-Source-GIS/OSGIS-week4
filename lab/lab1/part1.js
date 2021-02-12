301561ff7a749b28b28c187b325a6b9b3916c014
Zhijie (Alex) Zhou <zhou-zj@outlook.com>
2021-02-13 04:28:54 +0800

    Update part1.js

--- lab/lab1/part1.js
+++ lab/lab1/part1.js
@@ -66,7 +66,8 @@ Is printMenu a function? Answer this question with underscore. Should evaluate
 to true.
 ===================== */
 
-var query1;
+var query1 = _.isFunction(printMenu);
+
 
 console.log('printMenu is a function:', query1);
 
@@ -75,7 +76,7 @@ Is bakedGoods an array? Answer this question with underscore. Should evaluate
 to true.
 ===================== */
 
-var query2;
+var query2 = _.isArray(bakedGoods);
 
 console.log('bakedGoods is an array:', query2);
 
@@ -84,7 +85,7 @@ Is the first element in bakedGoods an object? Answer this question with
 underscore. Should evaluate to true.
 ===================== */
 
-var query3;
+var query3 = _.isObject(bakedGoods[0]);
 
 console.log('The first element in bakedGoods is an object:', query3);
 
@@ -92,7 +93,7 @@ console.log('The first element in bakedGoods is an object:', query3);
 Use _.where to return all cakes. Or bread. Whichever is your favorite.
 ===================== */
 
-var query4;
+var query4 = _.where(bakedGoods, {type: "Cake"});
 
 console.log('All bread. Or cakes:', query4);
 
@@ -100,7 +101,7 @@ console.log('All bread. Or cakes:', query4);
 Use _.filter to return all baked goods that cost more than $4.
 ===================== */
 
-var query5;
+var query5 = _.filter(bakedGoods, function(entry){return (entry.price >= 4)});
 
 console.log('More than $4:', query5);
 
@@ -108,7 +109,7 @@ console.log('More than $4:', query5);
 Use _.sortBy to order the list by inventory (from lowest to highest).
 ===================== */
 
-var query6;
+var query6 = _.sortBy(bakedGoods, "inventory");
 
 console.log('Sorted by inventory (lowest to highest):', query6);
 
@@ -116,7 +117,7 @@ console.log('Sorted by inventory (lowest to highest):', query6);
 Use _.groupBy to organize the baked goods by type.
 ===================== */
 
-var query7;
+var query7 = _.groupBy(bakedGoods, "type");
 
 console.log('Grouped by type:', query7);
 
@@ -139,7 +140,20 @@ Whole Wheat ... $4.49
 
 ===================== */
 
-// printMenuGroups(query7);
+var printMenuGroups = function(entry){
+  type = Object.keys(entry);
+  // for(var key in entry){
+  //   console.log(key);
+  //   _.each(entry[key], function(goods){console.log(goods.name + " ...  $" + goods.price)})
+  // };
+  _.each(type, function(type){
+    console.log(type);
+    _.each(entry[type], function(goods){
+      console.log(`${goods.name} ...  \$ ${goods.price}`)
+    })
+  })
+}
+printMenuGroups(query7);
 
 /* =====================
 We're probably writing each line of the menu with the code `food.name + " ... $" + food.price`.
