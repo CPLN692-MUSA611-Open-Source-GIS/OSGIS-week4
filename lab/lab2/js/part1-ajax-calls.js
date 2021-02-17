e47e3f5053212e5c4e7be59ddebe2e393ef615f3
jiaxuanlyu <70524710+jiaxuanlyu@users.noreply.github.com>
2021-02-17 18:57:52 +0800

    finish lab2 part1

--- lab/lab2/js/part1-ajax-calls.js
+++ lab/lab2/js/part1-ajax-calls.js
@@ -9,3 +9,23 @@ var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/ton
   maxZoom: 20,
   ext: 'png'
 }).addTo(map);
+
+
+var violetIcon = new L.Icon({
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png",
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+
+$.ajax("https://raw.githubusercontent.com/CPLN692-MUSA611-Open-Source-GIS/datasets/master/json/world-country-capitals.json"
+).done((res) => {
+  let country = JSON.parse(res)
+  console.log(country) //show data
+  country.map((i) => L.marker([i.CapitalLatitude, i.CapitalLongitude], {icon:violetIcon})
+  .bindPopup(`${i.CountryName}, ${i.ContinentName}`).openPopup().addTo(map)) //make markers
+  }
+)
