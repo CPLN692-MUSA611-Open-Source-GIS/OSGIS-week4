395da602d4b484cb53f85eb9d524a65ad4763330
kelly5265 <57802373+kelly5265@users.noreply.github.com>
2021-02-18 16:31:23 -0500

    Lab 1 & Lab2 pt1&2
    Attempts made for Lab 1 & Lab 2 pt1&2, but weird issues occurred

--- lab/lab2/js/part2-app-state.js
+++ lab/lab2/js/part2-app-state.js
@@ -33,19 +33,30 @@
 ===================== */
 
 // Use the data source URL from lab 1 in this 'ajax' function:
-var downloadData = $.ajax("http://");
+var downloadData = $.ajax("https://raw.githubusercontent.com/CPLN692-MUSA611-Open-Source-GIS/datasets/master/json/philadelphia-crime-snippet.json");
 
 // Write a function to prepare your data (clean it up, organize it
 // as you like, create fields, etc)
-var parseData = function() {};
+var parseData = function(rawdata) {
+  return JSON.parse(rawdata)
+};
 
 // Write a function to use your parsed data to create a bunch of
 // marker objects (don't plot them!)
-var makeMarkers = function() {};
+var makeMarkers = function(parsed) {
+  result=[]
+  parsed.forEach(add_new)
+  function add_new(item) {
+    result.push([item.Lat,item.Lng])
+  }
+  return result
+};
 
 // Now we need a function that takes this collection of markers
 // and puts them on the map
-var plotMarkers = function() {};
+var plotMarkers = function(markers) {
+  _.each(markers,L.marker(marker).addTo(map))
+};
 
 // At this point you should see a bunch of markers on your map if
 // things went well.
