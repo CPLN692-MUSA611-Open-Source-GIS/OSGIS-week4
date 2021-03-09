1acca73072b1f78dc65df3453e0d37099c6c7f1b
Ben Dodson <54688469+bndodson@users.noreply.github.com>
2021-03-09 07:15:37 -0500

    lab 1 lab 2

--- lab/lab2/js/part2-app-state.js
+++ lab/lab2/js/part2-app-state.js
@@ -33,19 +33,34 @@
 ===================== */
 
 // Use the data source URL from lab 1 in this 'ajax' function:
-var downloadData = $.ajax("http://");
+var downloadData = $.ajax("https://raw.githubusercontent.com/CPLN692-MUSA611-Open-Source-GIS/datasets/master/json/philadelphia-solar-installations.json");
 
 // Write a function to prepare your data (clean it up, organize it
 // as you like, create fields, etc)
-var parseData = function() {};
+var parseData = function(data) {
+    var parsed = JSON.parse(data);
+    var cleaned = parsed.map(point => {
+        point.LAT = Number(point.LAT);
+        point.LONG_ = Number(point.LONG_)
+        return point
+    })
+    return cleaned
+};
 
 // Write a function to use your parsed data to create a bunch of
 // marker objects (don't plot them!)
-var makeMarkers = function() {};
+var makeMarkers = function(data) {
+    return data.map(item => {
+        console.log(item)
+        return L.marker([item.LAT, item.LONG_])
+    });
+};
 
 // Now we need a function that takes this collection of markers
 // and puts them on the map
-var plotMarkers = function() {};
+var plotMarkers = function(data) {
+    return data.map(item => item.addTo(map));
+};
 
 // At this point you should see a bunch of markers on your map if
 // things went well.
@@ -66,22 +81,26 @@ var plotMarkers = function() {};
 
 // Look to the bottom of this file and try to reason about what this
 // function should look like
-var removeMarkers = function() {};
-
+var removeMarkers = function(data) {
+    setTimeout(function() {
+        console.log("Removing markers...");
+        return data.map(item => map.removeLayer(item));
+    }, 5000);
+};
 /* =====================
  Leaflet setup - feel free to ignore this
 ===================== */
 
 var map = L.map('map', {
-  center: [39.9522, -75.1639],
-  zoom: 14
+    center: [39.9522, -75.1639],
+    zoom: 14
 });
 var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
-  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  subdomains: 'abcd',
-  minZoom: 0,
-  maxZoom: 20,
-  ext: 'png'
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+    minZoom: 0,
+    maxZoom: 20,
+    ext: 'png'
 }).addTo(map);
 
 /* =====================
@@ -89,8 +108,8 @@ var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/ton
 ===================== */
 
 downloadData.done(function(data) {
-  var parsed = parseData(data);
-  var markers = makeMarkers(parsed);
-  plotMarkers(markers);
-  removeMarkers(markers);
+    var parsed = parseData(data);
+    var markers = makeMarkers(parsed);
+    plotMarkers(markers);
+    removeMarkers(markers);
 });
