cc72a87e338e9a05363414cab2d5c5f5a1a8e4b1
Haoheng-Tang <70991557+Haoheng-Tang@users.noreply.github.com>
2021-03-20 19:43:45 +0800

    lab 2 part 2 add_markers done

--- lab/lab2/js/part2-app-state.js
+++ lab/lab2/js/part2-app-state.js
@@ -32,20 +32,48 @@
        var one = justOne();
 ===================== */
 
+
+
 // Use the data source URL from lab 1 in this 'ajax' function:
-var downloadData = $.ajax("http://");
+var downloadData = $.ajax("http://raw.githubusercontent.com/CPLN692-MUSA611-Open-Source-GIS/datasets/master/json/world-country-capitals.json");
+console.log(downloadData)
 
 // Write a function to prepare your data (clean it up, organize it
 // as you like, create fields, etc)
-var parseData = function() {};
+
+
+var parseData = function(res) {  
+  let parsed = JSON.parse(res)
+  return parsed
+};
+
+
+//parseData(downloadData)
 
 // Write a function to use your parsed data to create a bunch of
 // marker objects (don't plot them!)
-var makeMarkers = function() {};
+var makeMarkers = function(parsed) { 
+  var markerlist = []
+  parsed.forEach(function(capital){  
+    lat = capital.CapitalLatitude;
+    lng = capital.CapitalLongitude;
+    Content = capital.CapitalName + ", \n" + capital.CountryName+ ", \n" + capital.ContinentName;
+    marker = [[lat, lng],Content]
+    markerlist.push(marker)
+  })
+  console.log(markerlist)
+  return markerlist
+}
+
 
 // Now we need a function that takes this collection of markers
 // and puts them on the map
-var plotMarkers = function() {};
+var plotMarkers = function(markerlist) {
+  markerlist.forEach(function(m){
+    L.marker(m[0]).addTo(map).bindPopup(m[1]).openPopup()
+  })
+
+};
 
 // At this point you should see a bunch of markers on your map if
 // things went well.
@@ -72,9 +100,14 @@ var removeMarkers = function() {};
  Leaflet setup - feel free to ignore this
 ===================== */
 
+//var map = L.map('map', {
+//  center: [39.9522, -75.1639],
+//  zoom: 14
+//});
+
 var map = L.map('map', {
   center: [39.9522, -75.1639],
-  zoom: 14
+  zoom: 2
 });
 var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -92,5 +125,5 @@ downloadData.done(function(data) {
   var parsed = parseData(data);
   var markers = makeMarkers(parsed);
   plotMarkers(markers);
-  removeMarkers(markers);
+  //removeMarkers(markers);
 });
