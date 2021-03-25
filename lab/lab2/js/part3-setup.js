286fc3298cf1c1bac898a2bf76c68c49f3d1b6ca
Haoheng-Tang <70991557+Haoheng-Tang@users.noreply.github.com>
2021-03-25 20:50:01 +0800

    lab 2 part 3 done

--- lab/lab2/js/part3-setup.js
+++ lab/lab2/js/part3-setup.js
@@ -7,7 +7,7 @@
   These are global variables that we can use throughout our application.
 ===================== */
 var myData;
-var myMarkers = [L.marker([39.9522, -75.1639])];
+var myMarkers = [];
 var numericField1, numericField2, booleanField, stringField;
 
 /* =====================
@@ -15,7 +15,7 @@ var numericField1, numericField2, booleanField, stringField;
 ===================== */
 var map = L.map('map', {
   center: [39.9522, -75.1639],
-  zoom: 14
+  zoom: 2
 });
 var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
